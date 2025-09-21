525554e885c5e8b415c318a60d21a270bb9fe136
gilangarta06 <gilangarta26@gmail.com>
2025-09-21 22:32:50 +0700

    feat : update accounts

--- lib/models/Accounts.ts
+++ lib/models/Accounts.ts
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 
 const AccountSchema = new mongoose.Schema({
   product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-  variant_name: { type: String, required: true }, // <-- ini wajib ada
+  variant_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   is_sold: { type: Boolean, default: false },
 });
 
-export default mongoose.models.Account ||
-  mongoose.model("Account", AccountSchema);
+// Pisahkan assignment supaya TypeScript tidak error
+let Account;
+
+if (mongoose.models.Account) {
+  Account = mongoose.models.Account;
+} else {
+  Account = mongoose.model("Account", AccountSchema);
+}
+
+export default Account;
