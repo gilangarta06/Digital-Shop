1362006605aa0171b31a01dae7e14591288c4d46
gilangarta06 <gilangarta26@gmail.com>
2025-09-25 14:24:27 +0700

    feat : fix sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -444,19 +444,19 @@ const SidebarGroupLabel = React.forwardRef<
 >(({ className, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "div"
 
-return (
+  return (
   <Comp
     // @ts-ignore
     ref={ref}
-    data-sidebar="group-label"
+    data-sidebar="group-action"
     className={cn(
-      "flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opacity] duration-200 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+      "absolute right-3 top-3.5 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-sidebar-foreground outline-none ring-sidebar-ring transition-transform hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
       className
     )}
     {...props}
   />
 )
-})
+})  
 SidebarGroupLabel.displayName = "SidebarGroupLabel"
 
 const SidebarGroupAction = React.forwardRef<
